752c0b0f5dc97f262295611860a73e46aa9005dd
Julien Landuré <julien.landure@gmail.com>
2017-04-04 18:07:36 +0200

    Update gcloud-deploy.sh

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,7 +1,6 @@
-google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
 google-cloud-sdk/bin/gcloud config set app/promote_by_default false
 echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
 echo "Deploying your app"
-google-cloud-sdk/bin/gcloud --project $APPNAME preview app deploy --version $VERSION --quiet ./app.yaml
+google-cloud-sdk/bin/gcloud --project $APPNAME app deploy --version $VERSION --quiet ./app.yaml
